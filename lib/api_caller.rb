6721af85d974eb66f15b9ffcd9eec195f1199c80
nautilus@barracuda <nautilus@shoppydoo.com>
2016-07-29 12:06:12 +0200

    keywords arguments

--- lib/api_caller.rb
+++ lib/api_caller.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 class ApiCaller
 
-  def self.get(uri_string, logger = nil)
+  def self.get(uri_string:, logger: nil)
     response = nil
 
     uri = URI(uri_string)
@@ -18,13 +18,13 @@ class ApiCaller
     response.is_a?(Net::HTTPSuccess) ? response.body.force_encoding(Encoding::UTF_8) : nil
   end
 
-  def self.post(uri_string, params, content_type = {'Content-Type' => 'application/json'}, logger = nil)
+  def self.post(uri_string:, params:, content_type: nil, logger: nil)
     response = nil
 
     uri = URI(uri_string)
     begin
       Net::HTTP.start(uri.host, uri.port, {use_ssl: uri.scheme == 'https', open_timeout: 1}) do |http|
-        request = Net::HTTP::Post.new(uri, content_type)
+        request = content_type ? Net::HTTP::Post.new(uri, content_type) : Net::HTTP::Post.new(uri)
         request.body = params
         response = http.request(request)
       end
--- spec/lib/api_caller_spec.rb
+++ spec/lib/api_caller_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe ApiCaller do
   describe '.get' do
 
     it 'should perform a http get' do
-      response = described_class.get('http://www.trovaprezzi.it/')
+      response = described_class.get(uri_string: 'http://www.trovaprezzi.it/')
 
       expect(response).to include('Trovaprezzi')
     end
 
     it 'should perform a https get' do
-      response = described_class.get('https://rubygems.org')
+      response = described_class.get(uri_string: 'https://rubygems.org')
 
       expect(response).to include('RubyGems')
     end
@@ -20,13 +20,13 @@ RSpec.describe ApiCaller do
     it 'should return nil when resource is not available' do
       allow_any_instance_of(Net::HTTP).to receive(:request).and_raise(Timeout::Error)
 
-      response = described_class.get('https://www.trovaprezzi.it/')
+      response = described_class.get(uri_string: 'https://www.trovaprezzi.it/')
 
       expect(response).to be_nil
     end
 
     it 'should return nil when page not found' do
-      response = described_class.get('https://www.trovaprezzi.it/404')
+      response = described_class.get(uri_string: 'https://www.trovaprezzi.it/404')
 
       expect(response).to be_nil
     end
@@ -35,7 +35,7 @@ RSpec.describe ApiCaller do
       logger = double(error: 'print error')
 
       expect(logger).to receive(:error)
-      response = described_class.get('http://vvv.trovaprezzi.it/', logger)
+      response = described_class.get(uri_string: 'http://vvv.trovaprezzi.it/', logger: logger)
 
       expect(response).to be_nil
     end
@@ -49,9 +49,19 @@ RSpec.describe ApiCaller do
       uri = URI('http://www.google.com/')
       expect(Net::HTTP::Post).to receive(:new).with(uri, {'Content-Type' => 'application/json'}).and_return(request)
 
-      described_class.post('http://www.google.com/', '{"key": "value"}')
+      described_class.post(uri_string: 'http://www.google.com/', params: '{"key": "value"}', content_type: {'Content-Type' => 'application/json'})
 
       expect(request.body).to eq('{"key": "value"}')
+    end
+
+    it 'should perform a standard http post' do
+      request = OpenStruct.new(body: 'request body')
+      uri = URI('http://www.google.com/')
+      expect(Net::HTTP::Post).to receive(:new).with(uri).and_return(request)
+
+      described_class.post(uri_string: 'http://www.google.com/', params: {key: "value"})
+
+      expect(request.body).to eq(:key => "value")
     end
 
   end
